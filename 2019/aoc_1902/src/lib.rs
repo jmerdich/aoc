30372811a72b5f19bbc5e73eadd47c24bb6098d1
Jake Merdich <jake@merdich.com>
2019-12-03 07:22:47 +0000

    Do day 2

---
+++ 2019/aoc_1902/src/lib.rs
@@ -0,0 +1,251 @@
+/* --- Day 2: 1202 Program Alarm ---
+
+On the way to your gravity assist around the Moon, your ship computer beeps
+angrily about a "1202 program alarm". On the radio, an Elf is already
+explaining how to handle the situation: "Don't worry, that's perfectly norma--"
+The ship computer bursts into flames.
+
+You notify the Elves that the computer's magic smoke seems to have escaped.
+"That computer ran Intcode programs like the gravity assist program it was
+working on; surely there are enough spare parts up there to build a new Intcode
+computer!"
+
+An Intcode program is a list of integers separated by commas (like 1,0,0,3,99).
+To run one, start by looking at the first integer (called position 0). Here,
+you will find an opcode - either 1, 2, or 99. The opcode indicates what to do;
+for example, 99 means that the program is finished and should immediately halt.
+Encountering an unknown opcode means something went wrong.
+
+Opcode 1 adds together numbers read from two positions and stores the result in
+a third position. The three integers immediately after the opcode tell you
+these three positions - the first two indicate the positions from which you
+should read the input values, and the third indicates the position at which the
+output should be stored.
+
+For example, if your Intcode computer encounters 1,10,20,30, it should read the
+values at positions 10 and 20, add those values, and then overwrite the value
+at position 30 with their sum.
+
+Opcode 2 works exactly like opcode 1, except it multiplies the two inputs
+instead of adding them. Again, the three integers after the opcode indicate
+where the inputs and outputs are, not their values.
+
+Once you're done processing an opcode, move to the next one by stepping forward
+4 positions.
+
+For example, suppose you have the following program:
+
+1,9,10,3,2,3,11,0,99,30,40,50
+
+For the purposes of illustration, here is the same program split into multiple
+lines:
+
+1,9,10,3, 2,3,11,0, 99, 30,40,50
+
+The first four integers, 1,9,10,3, are at positions 0, 1, 2, and 3. Together,
+they represent the first opcode (1, addition), the positions of the two inputs
+(9 and 10), and the position of the output (3). To handle this opcode, you
+first need to get the values at the input positions: position 9 contains 30,
+and position 10 contains 40. Add these numbers together to get 70. Then, store
+this value at the output position; here, the output position (3) is at position
+3, so it overwrites itself. Afterward, the program looks like this:
+
+1,9,10,70, 2,3,11,0, 99, 30,40,50
+
+Step forward 4 positions to reach the next opcode, 2. This opcode works just
+like the previous, but it multiplies instead of adding. The inputs are at
+positions 3 and 11; these positions contain 70 and 50 respectively. Multiplying
+these produces 3500; this is stored at position 0:
+
+3500,9,10,70, 2,3,11,0, 99, 30,40,50
+
+Stepping forward 4 more positions arrives at opcode 99, halting the program.
+
+Here are the initial and final states of a few more small programs:
+
+    1,0,0,0,99 becomes 2,0,0,0,99 (1 + 1 = 2).  2,3,0,3,99 becomes 2,3,0,6,99
+(3 * 2 = 6).  2,4,4,5,99,0 becomes 2,4,4,5,99,9801 (99 * 99 = 9801).
+1,1,1,4,99,5,6,0,99 becomes 30,1,1,4,2,5,6,0,99.
+
+Once you have a working computer, the first step is to restore the gravity
+assist program (your puzzle input) to the "1202 program alarm" state it had
+just before the last computer caught fire. To do this, before running the
+program, replace position 1 with the value 12 and replace position 2 with the
+value 2. What value is left at position 0 after the program halts?
+
+*/
+
+#[allow(dead_code)] // since mem::transmute can construct even without naming it....
+#[repr(transparent)]
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+struct Op(u32);
+
+impl Op {
+    const ADD: Op = Op { 0: 1 };
+    const MULT: Op = Op { 0: 2 };
+    const ENDPGM: Op = Op { 0: 99 };
+    const fn from_u32(value: u32) -> Op {
+        Op { 0: value }
+    }
+
+    const fn as_u32(self: Op) -> u32 {
+        self.0
+    }
+}
+
+// Output: new idx
+fn aoc2a_handle_add(idx: usize, tape: &mut [u32]) -> usize {
+    assert_eq!(tape[idx], Op::ADD.as_u32());
+    assert!(tape.len() >= idx + 4);
+    let loc_1 = tape[idx + 1] as usize;
+    let loc_2 = tape[idx + 2] as usize;
+    let loc_dst = tape[idx + 3] as usize;
+    //println!("ADD: {}@{} + {}@{} => {}@{}", tape[loc_1], loc_1, tape[loc_2], loc_2, tape[loc_1] + tape[loc_2], loc_dst);
+
+    tape[loc_dst] = tape[loc_1] + tape[loc_2];
+
+    idx + 4
+}
+// Output: new idx
+fn aoc2a_handle_mult(idx: usize, tape: &mut [u32]) -> usize {
+    assert_eq!(tape[idx], Op::MULT.as_u32());
+    assert!(tape.len() >= idx + 4);
+    let loc_1 = tape[idx + 1] as usize;
+    let loc_2 = tape[idx + 2] as usize;
+    let loc_dst = tape[idx + 3] as usize;
+    //println!("MULT: {}@{} * {}@{} => {}@{}", tape[loc_1], loc_1, tape[loc_2], loc_2, tape[loc_1] * tape[loc_2], loc_dst);
+
+    tape[loc_dst] = tape[loc_1] * tape[loc_2];
+
+    idx + 4
+}
+
+pub fn aoc2a_run(mut tape: Vec<u32>) -> Vec<u32> {
+    let mut pc: usize = 0;
+    loop {
+        match Op::from_u32(tape[pc]) {
+            Op::ADD => pc = aoc2a_handle_add(pc, &mut tape),
+            Op::MULT => pc = aoc2a_handle_mult(pc, &mut tape),
+            Op::ENDPGM => break,
+            _ => panic!(),
+        }
+    }
+    tape
+}
+
+/*
+
+"Good, the new computer seems to be working correctly! Keep it nearby during
+this mission - you'll probably use it again. Real Intcode computers support
+many more features than your new one, but we'll let you know what they are as
+you need them."
+
+"However, your current priority should be to complete your gravity assist
+around the Moon. For this mission to succeed, we should settle on some
+terminology for the parts you've already built."
+
+Intcode programs are given as a list of integers; these values are used as the
+initial state for the computer's memory. When you run an Intcode program, make
+sure to start by initializing memory to the program's values. A position in
+memory is called an address (for example, the first value in memory is at
+"address 0").
+
+Opcodes (like 1, 2, or 99) mark the beginning of an instruction. The values
+used immediately after an opcode, if any, are called the instruction's
+parameters. For example, in the instruction 1,2,3,4, 1 is the opcode; 2, 3, and
+4 are the parameters. The instruction 99 contains only an opcode and has no
+parameters.
+
+The address of the current instruction is called the instruction pointer; it
+starts at 0. After an instruction finishes, the instruction pointer increases
+by the number of values in the instruction; until you add more instructions to
+the computer, this is always 4 (1 opcode + 3 parameters) for the add and
+multiply instructions. (The halt instruction would increase the instruction
+pointer by 1, but it halts the program instead.)
+
+"With terminology out of the way, we're ready to proceed. To complete the
+gravity assist, you need to determine what pair of inputs produces the output
+19690720."
+
+The inputs should still be provided to the program by replacing the values at
+addresses 1 and 2, just like before. In this program, the value placed in
+address 1 is called the noun, and the value placed in address 2 is called the
+verb. Each of the two input values will be between 0 and 99, inclusive.
+
+Once the program has halted, its output is available at address 0, also just
+like before. Each time you try a pair of inputs, make sure you first reset the
+computer's memory to the values in the program (your puzzle input) - in other
+words, don't reuse memory from a previous attempt.
+
+Find the input noun and verb that cause the program to produce the output
+19690720. What is 100 * noun + verb? (For example, if noun=12 and verb=2, the
+answer would be 1202.)
+*/
+
+use std::cmp::min;
+pub fn aoc2b_run(needle: u32, tape: &[u32]) -> Option<(u32, u32)> {
+    for noun in 0..min(100, tape.len() as u32) {
+        for verb in 0..min(100, tape.len() as u32) {
+            let mut this_tape: Vec<u32> = tape.to_vec();
+            this_tape[1] = noun;
+            this_tape[2] = verb;
+            let res = aoc2a_run(this_tape);
+            if res[0] == needle {
+                return Some((noun, verb));
+            }
+        }
+    }
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn aoc2a_eg() {
+        assert_eq!(aoc2a_run(vec!(1, 0, 0, 0, 99)), vec!(2, 0, 0, 0, 99));
+        assert_eq!(aoc2a_run(vec!(2, 3, 0, 3, 99)), vec!(2, 3, 0, 6, 99));
+        assert_eq!(
+            aoc2a_run(vec!(2, 4, 4, 5, 99, 00)),
+            vec!(2, 4, 4, 5, 99, 9801)
+        );
+        assert_eq!(
+            aoc2a_run(vec!(1, 1, 1, 4, 99, 5, 6, 0, 99)),
+            vec!(30, 1, 1, 4, 2, 5, 6, 0, 99)
+        );
+        assert_eq!(
+            aoc2a_run(vec!(1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50)),
+            vec!(3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50)
+        );
+    }
+    #[test]
+    fn aoc2a_prob() {
+        let in_nums: Result<Vec<u32>, std::num::ParseIntError> = include_str!("test_input.txt")
+            .lines()
+            .collect::<String>()
+            .split(",")
+            .map(|s| s.parse::<u32>())
+            .collect();
+        let mut in_nums = in_nums.unwrap();
+        in_nums[1] = 12;
+        in_nums[2] = 2;
+        let res = aoc2a_run(in_nums);
+        assert_eq!(res[0], 5290681);
+    }
+    #[test]
+    fn aoc2b_eg() {
+        assert_eq!(aoc2b_run(99, &vec!(2, 0, 0, 0, 99)), Some((1, 4)));
+    }
+    #[test]
+    fn aoc2b_prob() {
+        let in_nums: Result<Vec<u32>, std::num::ParseIntError> = include_str!("test_input.txt")
+            .lines()
+            .collect::<String>()
+            .split(",")
+            .map(|s| s.parse::<u32>())
+            .collect();
+        let mut in_nums = in_nums.unwrap();
+        let res = aoc2b_run(19690720, &in_nums).unwrap();
+        assert_eq!(res, (57, 41));
+    }
+}

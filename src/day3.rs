dc2103048624ea72bed8b5436c6c7f3e5e48998f
Jake Merdich <jake@merdich.com>
2020-12-03 10:58:44 -0500

    Day 3 Part 1

---
+++ src/day3.rs
@@ -0,0 +1,113 @@
+#[derive(PartialEq, Eq, Clone, Copy)]
+pub enum GridSq {
+    Ground,
+    Tree,
+}
+
+pub struct Map {
+    width: usize,
+    height: usize,
+    terrain: Vec<Vec<GridSq>>,
+}
+
+impl Map {
+    fn at(&self, x: usize, y: usize) -> Option<GridSq> {
+        if y >= self.height {
+            return None;
+        }
+        Some(self.terrain[y][x % self.width])
+    }
+    fn at_loc(&self, loc: (usize, usize)) -> Option<GridSq> {
+        self.at(loc.0, loc.1)
+    }
+}
+
+#[aoc_generator(day3)]
+pub fn input_generator(input: &str) -> Map {
+    let terrain: Vec<Vec<GridSq>> = input
+        .lines()
+        .map(|l| {
+            l.chars()
+                .map(|c| match c {
+                    '.' => GridSq::Ground,
+                    '#' => GridSq::Tree,
+                    _ => panic!(),
+                })
+                .collect()
+        })
+        .collect();
+    assert!(!terrain.is_empty());
+    let width = terrain[0].len();
+    assert!(width > 0);
+    assert!(terrain.iter().all(|v| v.len() == width));
+
+    Map {
+        width,
+        height: terrain.len(),
+        terrain,
+    }
+}
+
+#[aoc(day3, part1)]
+pub fn solve_part1(input: &Map) -> usize {
+    let mut loc: (usize, usize) = (0, 0);
+    let mut hits = 0;
+    loop {
+        match input.at_loc(loc) {
+            Some(GridSq::Ground) => {}
+            Some(GridSq::Tree) => {
+                hits += 1;
+            }
+            None => {
+                break;
+            }
+        }
+        loc = (loc.0 + 3, loc.1 + 1);
+    }
+    hits
+}
+
+#[aoc(day3, part2)]
+pub fn solve_part2(input: &Map) -> usize {
+    0
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const EG_INPUT: &str = "\
+..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#";
+    const INPUT: &str = include_str!("../input/2020/day3.txt");
+
+    #[test]
+    fn eg_part1() {
+        let content = input_generator(EG_INPUT);
+        assert_eq!(solve_part1(&content), 7);
+    }
+    #[test]
+    fn eg_part2() {
+        let content = input_generator(EG_INPUT);
+        assert_eq!(solve_part2(&content), 0);
+    }
+    #[test]
+    fn part1() {
+        let content = input_generator(INPUT);
+        assert_eq!(solve_part1(&content), 289);
+    }
+    #[test]
+    fn part2() {
+        let content = input_generator(INPUT);
+        assert_eq!(solve_part2(&content), 0);
+    }
+}

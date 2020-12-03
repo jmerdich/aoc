e8b34f1df1160cef361ebb71b8116e517de6b266
Jake Merdich <jake@merdich.com>
2020-12-02 20:59:52 -0500

    Day 1 Part 2

--- src/day1.rs
+++ src/day1.rs
@@ -15,20 +15,46 @@ pub fn solve_part1(input: &[i64]) -> i64 {
     panic!("No solution?");
 }
 
+#[aoc(day1, part2)]
+pub fn solve_part2(input: &[i64]) -> i64 {
+    for i in 0..input.len() {
+        let v_i = input[i];
+        for j in (i + 1)..input.len() {
+            let v_j = input[j];
+            for k in (j + 1)..input.len() {
+                let v_k = input[k];
+                if v_i + v_j + v_k == 2020 {
+                    return v_i * v_j * v_k;
+                }
+            }
+        }
+    }
+    panic!("No solution?");
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
-    #[test]
-    fn eg_part1() {
-        let input = "\
+
+    const EG_INPUT: &str= "\
 1721
 979
 366
 299
 675
 1456";
-        let content = input_generator(input);
+
+    #[test]
+    fn eg_part1() {
+        let content = input_generator(EG_INPUT);
         let res = solve_part1(&content);
         assert_eq!(res, 514579);
+    }
+
+    #[test]
+    fn eg_part2() {
+        let content = input_generator(EG_INPUT);
+        let res = solve_part2(&content);
+        assert_eq!(res, 241861950);
     }
 }

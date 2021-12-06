26856577783b4c4e7da7e5616bc43aa06a0e3ef3
Jake Merdich <jake@merdich.com>
2021-12-06 00:53:16 -0500

    Day 6 done

--- 2021/src/day6.rs
+++ 2021/src/day6.rs
@@ -1,23 +1,61 @@
 #![allow(unused_variables, dead_code)]
 
+#[derive(Debug, Clone)]
 pub struct Content {
-    pub ages: Vec<u32>
+    pub counts: [u64; 10]
 }
 
 #[aoc_generator(day6)]
 pub fn input_generator(input: &str) -> Content {
-    let ages: Vec<u32> = input.split(",").map(|n| n.trim().parse().unwrap()).collect();
-    Content { ages }
+    let ages: Vec<u64> = input.split(",").map(|n| n.trim().parse().unwrap()).collect();
+    let mut counts : [u64; 10] = [0; 10];
+    for n in 0..10 {
+        counts[n] = ages.iter().filter(|v| **v == n as u64).count() as u64;
+    }
+    Content { counts }
+}
+
+pub fn count_zeros(input: &Content) -> usize {
+    input.counts[0] as usize
+}
+
+pub fn age(input: &mut Content) {
+    let mut counts : [u64; 10] = [0; 10];
+    counts[6] += input.counts[0];
+    for n in 1..10 {
+        counts[n-1] += input.counts[n];
+    };
+    input.counts = counts;
+}
+
+pub fn spawn_new(input: &mut Content, count: usize) {
+    input.counts[8] += count as u64;
+}
+
+pub fn sim_fish(input: &mut Content, days: usize) {
+    for day in 0..days {
+        let to_add = count_zeros(input);
+        age(input);
+        spawn_new(input, to_add);
+    }
+}
+
+pub fn count(input: &Content) -> usize {
+    input.counts.iter().sum::<u64>() as usize
 }
 
 #[aoc(day6, part1)]
 pub fn solve_part1(input: &Content) -> usize {
-    0
+    let mut input = input.clone();
+    sim_fish(&mut input, 80);
+    count(&input)
 }
 
 #[aoc(day6, part2)]
 pub fn solve_part2(input: &Content) -> usize {
-    0
+    let mut input = input.clone();
+    sim_fish(&mut input, 256);
+    count(&input)
 }
 
 #[cfg(test)]
@@ -32,21 +70,21 @@ mod test {
     #[test]
     fn eg_part1() {
         let content = input_generator(EG_INPUT);
-        assert_eq!(solve_part1(&content), 0);
+        assert_eq!(solve_part1(&content), 5934);
     }
     #[test]
     fn eg_part2() {
         let content = input_generator(EG_INPUT);
-        assert_eq!(solve_part2(&content), 0);
+        assert_eq!(solve_part2(&content), 26984457539);
     }
     #[test]
     fn part1() {
         let content = input_generator(INPUT);
-        assert_eq!(solve_part1(&content), 0);
+        assert_eq!(solve_part1(&content), 380243);
     }
     #[test]
     fn part2() {
         let content = input_generator(INPUT);
-        assert_eq!(solve_part2(&content), 0);
+        assert_eq!(solve_part2(&content), 1708791884591);
     }
 }

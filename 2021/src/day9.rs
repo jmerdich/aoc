3698c0df00ba004e191e70264bc850895fd3b689
Jake Merdich <jake@merdich.com>
2021-12-12 16:49:48 -0500

    Day 9 done

--- 2021/src/day9.rs
+++ 2021/src/day9.rs
@@ -7,27 +7,65 @@ pub struct Content {
 }
 
 impl Content {
-    fn adjacent(&self, x: usize, y: usize) -> Vec<u8> {
+    fn adjacent_vals(&self, x: usize, y: usize) -> Vec<u8> {
+        self.adjacent(x, y)
+            .iter()
+            .map(|(x, y)| self.grid.get(*y, *x).unwrap())
+            .copied()
+            .collect()
+    }
+    fn adjacent(&self, x: usize, y: usize) -> Vec<(usize, usize)> {
         let mut adjacent = Vec::new();
 
         if y > 0 {
-            if let Some(v) = self.grid.get(y - 1, x) {
-                adjacent.push(*v);
-            }
+            adjacent.push((x, y - 1));
         }
-        if let Some(v) = self.grid.get(y + 1, x) {
-            adjacent.push(*v);
+        if y + 1 < self.grid.rows() {
+            adjacent.push((x, y + 1));
         }
         if x > 0 {
-            if let Some(v) = self.grid.get(y, x - 1) {
-                adjacent.push(*v);
-            }
+            adjacent.push((x - 1, y));
         }
-        if let Some(v) = self.grid.get(y, x + 1) {
-            adjacent.push(*v);
+        if x + 1 < self.grid.cols() {
+            adjacent.push((x + 1, y));
         }
 
         adjacent
+    }
+
+    fn find_mins(&self) -> Vec<(usize, usize)> {
+        let mut items = Vec::new();
+
+        for x in 0..self.grid.cols() {
+            for y in 0..self.grid.rows() {
+                let v = *self.grid.get(y, x).unwrap();
+                if v < *self.adjacent_vals(x, y).iter().min().unwrap() {
+                    items.push((x, y));
+                }
+            }
+        }
+
+        items
+    }
+
+    fn find_pool_size_for_point(&self, x: usize, y: usize) -> usize {
+        let mut filled = Grid::init(self.grid.rows(), self.grid.cols(), false);
+
+        self.fill_from_point(x, y, &mut filled);
+
+        filled.iter().filter(|v| **v).count()
+    }
+
+    fn fill_from_point(&self, x: usize, y: usize, mask: &mut Grid<bool>) {
+        if *mask.get(y, x).unwrap() || (*self.grid.get(y, x).unwrap() == 9) {
+            return;
+        }
+
+        *mask.get_mut(y, x).unwrap() = true;
+
+        for (px, py) in self.adjacent(x, y) {
+            self.fill_from_point(px, py, mask);
+        }
     }
 }
 
@@ -46,21 +84,32 @@ pub fn input_generator(input: &str) -> Content {
 
 #[aoc(day9, part1)]
 pub fn solve_part1(input: &Content) -> usize {
-    let mut risk: usize = 0;
-    for x in 0..input.grid.cols() {
-        for y in 0..input.grid.rows() {
-            let v = *input.grid.get(y, x).unwrap();
-            if v < *input.adjacent(x, y).iter().min().unwrap() {
-                risk += v as usize + 1;
-            }
-        }
-    }
-    risk
+    input
+        .find_mins()
+        .iter()
+        .map(|(x, y)| (*input.grid.get(*y, *x).unwrap() + 1) as usize)
+        .sum()
 }
 
 #[aoc(day9, part2)]
 pub fn solve_part2(input: &Content) -> usize {
-    0
+    let mut pool_sizes: Vec<usize> = input
+        .find_mins()
+        .iter()
+        .map(|(x, y)| input.find_pool_size_for_point(*x, *y))
+        .collect();
+
+    pool_sizes.sort_unstable();
+
+    let mut res = 1;
+
+    for _ in 0..3 {
+        if let Some(biggest) = pool_sizes.pop() {
+            res *= biggest;
+        }
+    }
+
+    res
 }
 
 #[cfg(test)]
@@ -83,7 +132,7 @@ mod test {
     #[test]
     fn eg_part2() {
         let content = input_generator(EG_INPUT);
-        assert_eq!(solve_part2(&content), 0);
+        assert_eq!(solve_part2(&content), 1134);
     }
     #[test]
     fn part1() {
@@ -93,6 +142,6 @@ mod test {
     #[test]
     fn part2() {
         let content = input_generator(INPUT);
-        assert_eq!(solve_part2(&content), 0);
+        assert_eq!(solve_part2(&content), 1023660);
     }
 }
